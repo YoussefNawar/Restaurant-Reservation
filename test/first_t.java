a95d7a9fad9d423eaec8497dfd8c6c3d8713d392
Youssef Nawar <youssef.nawar10@yahoo.com>
2020-03-19 15:23:45 -0700

    added test file path and class

---
+++ test/first_t.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class first_t {
+
+	@Test
+	void test() {
+		Assertions.assertEquals(new String("12"), new String("12"));
+	}
+
+}
